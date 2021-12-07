fd78f6db0f958d20018dc470e968f56c3ca0ba50
findcore <cvb8306@naver.com>
2021-12-07 22:18:02 +0900

    'third'

--- web/api/views.py
+++ web/api/views.py
@@ -1,6 +1,9 @@
+import json
+
 from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from pytz import unicode
 
 from trans.models import FileUpload
 
@@ -9,14 +12,17 @@ from trans.models import FileUpload
 @csrf_exempt
 def fileUpload(request):
     if request.method == 'POST':
-        title = 11
+        title = 45
         img = request.FILES["image"]
         fileupload = FileUpload(
             title=title,
             imgfile=img,
         )
         fileupload.save()
-        filesend=FileUpload.objects.filter(title__contains=10).first()
-        filesend2=model_to_dict(filesend)
+        filesend = FileUpload.objects.get(title__contains=45)
+        filesend2 = filesend.__dict__
 
-        return JsonResponse(data=filesend2, status=201)
+
+        del(filesend2['_state'])
+        filesend2=json.dumps(filesend2)
+        return JsonResponse(data=filesend2, status=201,safe=False)
